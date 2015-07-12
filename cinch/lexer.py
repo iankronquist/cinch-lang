db8a979b6160a5bcd7d2916297976427abd65942
Ian Kronquist <iankronquist@teleport.com>
2015-07-12 16:53:08 -0700

    Code quality - flake8 & docstrings

--- cinch/lexer.py
+++ cinch/lexer.py
@@ -1,8 +1,11 @@
-
 # This is the lexer. We could build a state machine which would parse
 # each token character by character, but the point of this project is to
-# be as simple as possible, so we will literally just call string.split
-# source is the source code as a string
-# returns an array of string tokens.
+# be as simple as possible, so we will literally just split the string on
+# spaces, scrub all newlines, and filter out any empty strings
+
+
 def lex(source):
-    return map(lambda x: x.strip(), source.split(' '))
+    """Lex the source code. Split on spaces, strip newlines, and filter out
+    empty strings"""
+    return filter(lambda s: s != '',
+                  map(lambda x: x.strip(), source.split(' ')))
