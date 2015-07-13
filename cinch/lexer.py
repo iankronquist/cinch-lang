abf2f4209f8adf06bef624e9d0a188eba39c2c7a
Ian Kronquist <iankronquist@teleport.com>
2015-07-12 17:20:29 -0700

    Remove comments as part of lexing

--- cinch/lexer.py
+++ cinch/lexer.py
@@ -1,3 +1,5 @@
+import re
+
 # This is the lexer. We could build a state machine which would parse
 # each token character by character, but the point of this project is to
 # be as simple as possible, so we will literally just split the string on
@@ -7,5 +9,7 @@
 def lex(source):
     """Lex the source code. Split on spaces, strip newlines, and filter out
     empty strings"""
+    # Strip comments from source code.
+    source = re.sub('#.*$', '', source)
     return filter(lambda s: s != '',
                   map(lambda x: x.strip(), source.split(' ')))
