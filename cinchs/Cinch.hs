58aa5470ef43815d31f85be3933d70d0c6b3fa69
Ian Kronquist <iankronquist@teleport.com>
2015-09-27 20:25:40 -0700

    Add dump and lex subcommands

--- cinchs/Cinch.hs
+++ cinchs/Cinch.hs
@@ -5,23 +5,36 @@ import Lexer
 import Parser
 
 main = do
-  let usage = "Cinch [interpret|dump|vm|compile] file"
+  let usage = "Cinch [lex|interpret|dump|vm|compile] file"
   args <- getArgs
   if length args /= 2 then
     putStrLn usage
   else
-    case (args !! 0) of
+    case head args of
       "interpret" -> putStrLn "Not yet implemented."
       "dump"      -> dumpAst args
       "vm"        -> putStrLn "Not yet implemented."
       "compile"   -> putStrLn "Not yet implemented."
+      "lex"       -> dumpTokens args
+      otherwise   -> putStrLn usage
+
+
+dumpTokens args = do
+  itExists <- doesFileExist $ args !! 1
+  if itExists then do
+    source <- readFile $ args !! 1
+    let tokens = lexer source
+    print tokens
+  else
+    putStrLn $ "The file " ++ args !! 1 ++ " does not exist!"
 
 dumpAst args = do
   itExists <- doesFileExist $ args !! 1
   if itExists then do
     source <- readFile $ args !! 1
     let tokens = lexer source
-    let ast = parse tokens
-    putStrLn $ show ast
+    case parse tokens of
+      Nothing -> putStrLn $ "Failed to parse the file " ++ args !! 1
+      Just ast -> print ast
   else
     putStrLn $ "The file " ++ args !! 1 ++ " does not exist!"
